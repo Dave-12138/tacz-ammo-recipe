6b1b5aa126198d9adf05b4c3d3ed961fa7097030
Dave_12138 <207955131@qq.com>
2025-03-12 14:28:07 +0800

    处理溢出与缺项

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,17 @@ function deep(dpath: string): string[] {
     }).flat();
 }
 
+function mkAllDir(dpath: string) {
+    path.join(dpath).split(path.sep).reduce((base, cur) => {
+        const fullPath = path.join(base, cur);
+        if (!fs.existsSync(fullPath) && !cur.includes('.')) {
+            fs.mkdirSync(fullPath);
+        }
+        return fullPath;
+    }, ".")
+}
 function toNewPath(jsonPath: string) {
-    return jsonPath.replace(/(?<=[\\/]recipes[\\/])/, "create_ammo_recipe" + path.sep);
+    return jsonPath.replace(/(?<=[\\/]recipes?[\\/])/, "create_ammo_recipe" + path.sep);
 }
 type ItemData = {
     item: string;
@@ -52,11 +61,51 @@ interface CreateRecipe {
     results: CreateItemStack[];
 }
 function createRecipe(oldRecipe: TaczRecipe): CreateRecipe {
+    // 处理空值
+    oldRecipe.materials.forEach(e => e.count ?? (e.count = 1));
+    oldRecipe.result.count ?? (oldRecipe.result.count = 1);
+    // 约分
+    if (oldRecipe.materials.some(e => e.count > 16)) {
+        // console.log(oldRecipe.materials.map(i => i.count));
+        const minCount = Math.min(...oldRecipe.materials.map(e => e.count));
+        const commonFactors = Array.from<number>({ length: minCount }).fill(0).map((_, i) => oldRecipe.materials.every(e => e.count % (minCount - i) == 0) ? (minCount - i) : 1);
+        const commonFactor = Math.max(...commonFactors);
+        if (commonFactor > 1) {
+            oldRecipe.materials.forEach(m => m.count /= commonFactor);
+            oldRecipe.result.count /= commonFactor;
+            console.log("约分↓ 系数为 %d", commonFactor);
+        }
+    }
+    // 还大于64就尝试压块
+    if (oldRecipe.materials.some(e => e.count > 64)) {
+        const ingotsOrGemsPatt = /(?<=(?:forge|c):)(?:ingots|gems)\//;
+        oldRecipe.materials
+            .filter(e => Object.hasOwn(e.item, "tag") && ingotsOrGemsPatt.test(e.item["tag"]) && e.count > 64)
+            .forEach(e => {
+                const blockTag = e.item["tag"].replace(ingotsOrGemsPatt, "storage_blocks/");
+                const block = { item: { tag: blockTag }, count: Math.floor(e.count / 9) };
+                e.count %= 9;
+                oldRecipe.materials.push(block);
+                console.log("压块↓ %s → %s", e.item["tag"], blockTag);
+            });
+
+    }
+    // 最终策略：暴力除二，向上取整
+    if (oldRecipe.materials.some(e => e.count > 64)) {
+        while (oldRecipe.materials.some(e => e.count > 64)) {
+            oldRecipe.materials.forEach(e => e.count = Math.round(e.count /= 2));
+            oldRecipe.result.count = Math.round(oldRecipe.result.count / 2);
+            console.log("暴力除2↓");
+        }
+    }
+
+    // 锭换板
     oldRecipe.materials.forEach(l => {
         if (Object.hasOwn(l.item, "tag")) {
-            l.item["tag"] = (l.item['tag'] as string).replace("forge:ingots/copper", "forge:plates/copper")
+            l.item["tag"] = (l.item["tag"] as string).replace(/(?<=forge|c):ingots\/copper/, ":plates/copper")
         }
     })
+    // 构造机械动力配方
     const n: CreateRecipe = {
         type: "create:compacting",
         ingredients: oldRecipe.materials.map(e => Array.from<ItemData>({ length: e.count }).fill(e.item)).flat(),
@@ -65,20 +114,12 @@ function createRecipe(oldRecipe: TaczRecipe): CreateRecipe {
     return n;
 }
 
-function mkAllDir(dpath: string) {
-    path.join(dpath).split(path.sep).reduce((base, cur) => {
-        const fullPath = path.join(base, cur);
-        if (!fs.existsSync(fullPath) && !cur.includes('.')) {
-            fs.mkdirSync(fullPath);
-        }
-        return fullPath;
-    }, ".")
-}
-deep('.').filter(e => /recipes[\\/].*\.json/.test(e)).forEach(p => {
+deep('.').filter(e => /recipes?[\\/].*\.json/.test(e)).forEach(p => {
     const data = read(p) as TaczRecipe;
     if (data.type == "tacz:gun_smith_table_crafting" && data.result.type == "ammo") {
         const np = toNewPath(p);
         mkAllDir(np);
         write(np, createRecipe(data));
+        console.log("已完成： %s", np);
     }
 })
